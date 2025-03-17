0161b9b6091a2a88c852807f5e1fdc4f5887139d
KDaniel <khderdaniel.77@gmail.com>
2025-03-17 21:57:45 +0200

    15

---
+++ sixth.py
@@ -0,0 +1,9 @@
+class Queue:
+    def __init__(self):
+        self.items = []
+    def enqueue(self, item):
+        self.items.append(item)
+    def dequeue(self):
+        if not self.items:
+            raise IndexError("Queue is empty")
+        return self.items.pop()
