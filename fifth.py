acc370d5180c04a00d6f9a3f3f94d7586cd0b267
KDaniel <khderdaniel.77@gmail.com>
2025-03-22 14:43:23 +0200

    21

--- fifth.py
+++ fifth.py
@@ -1,10 +1,9 @@
-#In a stack, the last element we added  is the first one we removed.
-#last in first out (LIFO).
+#In a stack, the last element added (or “pushed”) is the first one to be removed (or “popped”).
+# This is known as the Last In, First Out principle (LIFO).
 # in a stack the add process called (pushed); the remove process called (popped).
-#a group of boxes stacked on top of each other
+#a group of boxes stacked on top of each other.
 
-
-#In a queue, the first element we added is the first one we removed.
-#first in first out (FIFO).
+#In a queue, the first element added (or “enqueued”) is the first one to be removed (or “dequeued”).
+# This follows the First In, First Out principle (FIFO).
 # in a queue the add process called (enqueue); the remove process called (dequeue).
-#the queue on gas station
+#the queue on gas station.
